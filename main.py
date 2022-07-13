9436365e5fb968bb7c71b0c63cd79a97ee07f481
irchamali <id.irchamali@gmail.com>
2022-07-13 22:35:19 +0700

    Make function for add params

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from sklearn import datasets #panggil lib scikitlearn
 
-st.title('Aplikasi Web Datamining')
+st.title('Aplikasi Web Data Mining')
 st.write("""
 # Algoritma KNN, SVM dan Random Forest
 Mana yang terbaik??
@@ -41,3 +41,22 @@ def pilih_dataset(nama):
 x, y = pilih_dataset(nama_dataset)
 st.write('Jumlah Baris dan kolom : ', x.shape)
 st.write('Jumlah kelas : ', len(np.unique(y)))
+
+# buat function untuk tambah parameter
+def tambah_parameter(nama_algortima):
+    params = dict()
+    if nama_algortima == 'KNN':
+        K = st.sidebar.slider('K', 1, 15)
+        params['K'] = K
+    elif nama_algortima == 'SVM':
+        C = st.sidebar.slider('C', 0.01, 10.0)
+        params['C'] = C
+    else:
+        max_depth = st.sidebar.slider('max_depth', 2, 15)
+        params['max_depth'] = max_depth
+        n_estimators = st.sidebar.slider('n_estimators', 1, 100)
+        params['n_estimators'] = n_estimators
+    return params
+
+# panggil fungsi params berdasarkan algoritma yg dipilih
+params = tambah_parameter(algoritma)
